6cb2416f2bd8dfce54f9b9a48c366e35c1c1f92f
Nguyen Tan Long <hitlong.dinho.89@gmail.com>
2024-08-31 09:18:42 +0700

    05 video 42

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TodoData from './component/todo/TodoData';
+import TodoNew from './component/todo/TodoNew';
+import './component/todo/todo.css';
+import reactLogo from './assets/react.svg';
+import { useState } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
+  const [todoList, setTodoList] = useState([
+    // { id: 1, name: "Learning React " },
+    // { id: 2, name: "Watching Youtube" }
+  ])
+
+  const addNewTodo = (name) => {
+    const newTodo = {
+      id: randomIntFromInterval(1, 1000000),
+      name1: name
+    }
+
+    setTodoList([...todoList, newTodo])
+  }
+
+  const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const deleteTodo = (thamsoIdtuTodoData) => {
+    const newTodo = todoList.filter(item => item.id !== thamsoIdtuTodoData);
+    setTodoList(newTodo);
+    // console.log(">>>> Check", thamsoIdtuTodoData);
+  }
+
+  //{key:value}
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello reactJS +Nguyễn Trường tương tư</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="todo-container">
+      <div className="todo-title">Todo List</div>
+      <TodoNew
+        addNewTodo={addNewTodo}
+      />
+      {todoList.length > 0 ?
+        <TodoData
+          todoList={todoList}
+          deleteTodo1={deleteTodo}
+        />
+        :
+        <div className='todo-image'>
+          <img src={reactLogo} className='logo' />
+        </div>
+      }
+    </div>
   )
 }
 
